732616e8f11710543378751b63ce1e1b8e5e1c82
Edna Omadjambe <ednaomadjambe@Ednas-Air.attlocal.net>
2023-01-02 15:58:11 -0500

    header and footer css files and imports

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
+import './footer.css';
 
 const Footer = () => {
   const location = useLocation();
--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import Auth from '../../utils/auth';
+import './header.css';
 
 const Header = () => {
   const logout = (event) => {
