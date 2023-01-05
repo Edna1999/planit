75cd73554e9aff1fe9ad991075db4abbf9685258
clbutl <cannonlbutler@gmail.com>
2023-01-05 11:23:40 -0500

    merge conflict changes 1

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -42,78 +42,6 @@ const Signup = () => {
   };
 
   return (
-    // <main className="flex-row justify-center mb-4">
-    //   <div className="col-12 col-lg-10">
-    //     <div className="card">
-    //       <h4 className="card-header bg-dark text-light p-2">Sign Up</h4>
-    //       <div className="card-body">
-    //         {data ? (
-    //           <p>
-    //             Success! You may now head{' '}
-    //             <Link to="/">back to the homepage.</Link>
-    //           </p>
-    //         ) : (
-    //           <form onSubmit={handleFormSubmit}>
-    //             <input
-    //               className="form-input"
-    //               placeholder="username"
-    //               name="username"
-    //               type="text"
-    //               value={formState.username}
-    //               onChange={handleChange}
-    //             />
-    //             <input
-                  // className="form-input"
-                  // placeholder="Your first name"
-                  // name="firstName"
-                  // type="text"
-                  // value={formState.firstName}
-                  // onChange={handleChange}
-    //             />
-    //             <input
-    //               className="form-input"
-                  // placeholder="Your last name"
-                  // name="lastName"
-                  // type="text"
-                  // value={formState.lastName}
-                  // onChange={handleChange}
-    //             />
-    //             <input
-    //               className="form-input"
-                  // placeholder="Your email"
-                  // name="email"
-                  // type="email"
-                  // value={formState.email}
-                  // onChange={handleChange}
-    //             />
-    //             <input
-    //               className="form-input"
-                  // placeholder="******"
-                  // name="password"
-                  // type="password"
-                  // value={formState.password}
-                  // onChange={handleChange}
-    //             />
-    //             <button
-    //               className="btn btn-block btn-primary"
-    //               style={{ cursor: 'pointer' }}
-    //               type="submit"
-    //             >
-    //               Submit
-    //             </button>
-    //           </form>
-    //         )}
-
-    //         {error && (
-    //           <div className="my-3 p-3 bg-danger text-white">
-    //             {error.message}
-    //           </div>
-    //         )}
-    //       </div>
-    //     </div>
-    //   </div>
-    // </main>
-
     <body>
       {data ? (
       <p>
@@ -180,11 +108,12 @@ const Signup = () => {
           </div>
           <div className="bottom-section">
           </div>
-          <h2 className="signup-h2">
+            <p className='or'>OR</p>
+          <h4 className="signup-h4">
             <Link to='/login'>
               Login Instead
             </Link>
-          </h2>
+          </h4>
         </form>
       </div>
       )}
